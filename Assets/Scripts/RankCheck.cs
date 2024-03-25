19203d1eba110e21e9a556a4cf23b7b43fc82773
김민결 <neomingyeol@gmail.com>
2024-03-25 16:31:27 +0900

    Bye Bye

---
+++ Assets/Scripts/RankCheck.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RankCheck : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> checks = new List<GameObject>();
+    private void Start()
+    {
+        for (int i = 0; i < checks.Count; i++)
+        {
+            
+        }
+    }
+}
