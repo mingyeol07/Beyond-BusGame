c7184f476cbaecdd10bba131ef2c0257de790cf1
sibottle <116580465+sibottle@users.noreply.github.com>
2024-09-13 15:23:25 +0900

    Sprite&Billboard SMall Update

--- Assets/GameAsset/1. Scripts/BillboardScript.cs
+++ Assets/GameAsset/1. Scripts/BillboardScript.cs
@@ -6,7 +6,6 @@ public class BillboardScript : MonoBehaviour
 {
     void LateUpdate()
     {
-        transform.rotation = Quaternion.Euler(Camera.main.transform.eulerAngles.x,Camera.main.transform.eulerAngles.y,transform.eulerAngles.z);
-            
+        transform.rotation = Quaternion.Euler(0,Camera.main.transform.eulerAngles.y,transform.eulerAngles.z);
     }
 }
